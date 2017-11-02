ea18c4358cee595894f60588e21205bac82367a9
KIMDOHYEONG <32589504+CPUDoHyeong@users.noreply.github.com>
2017-11-02 15:06:42 +0900

    Update Pet3Test.java

--- Pet3Test.java
+++ Pet3Test.java
@@ -1,9 +1,17 @@
+/* 
+ * 작성자 : 3조(김도형, 장연정, 성기진)
+ * 최초 작성일 : 2017.09.28
+ * 최종 작성일 : 2017.09.28
+ * 목적 : 도둑키우기 프로그램의 이미지 출력
+ * 개정 이력 : 김도형, 장연정, 성기진 / 2017.09.28
+ */
 
 public class Pet3Test {
 
 	public static void main(String[] args) {
-		Pet3 myPet = new Pet3();
-		myPet.game();
+		
+		Pet3_2 myPet = new Pet3_2(); // Pet3_2 객체 생성. myPet은 Pet3_2의 객체의 주소를 저장하고 있다
+		myPet.game();                // myPet이 참조하는 객체로 가서 game 이라는 메소드를 호출한다.
 	}
 
 }
